a12b7477a22ce1f5ba7a010897601ce20bbe3ade
HHH <237106102@qq.com>
2021-07-09 23:39:01 +0800

    读取图标与路径

--- src/MainWindows.py
+++ src/MainWindows.py
@@ -1,5 +1,11 @@
-from PySide2.QtWidgets import QApplication, QMessageBox
+import os
+
+import win32gui
+import win32ui
+from PIL import Image
+from PySide2.QtGui import QIcon
 from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QMessageBox, QTableWidgetItem
 from PySide2.QtWidgets import QFileDialog
 
 
@@ -41,8 +47,34 @@ class MainWindows:
     def init(self):
         # 初始化已保存的路径
         with open("../resource/config/path.txt", "r+", encoding="utf-8") as file:
-            for line in file:
-                self.ui.listWidget.addItem(line.replace("\n", ""))
+            for index, line in enumerate(file):
+                self.ui.appStoreTable.insertRow(index)
+                # 填充名字与图标
+                item = QTableWidgetItem(os.path.split(line)[1].strip(".exe\n"))
+                item.setIcon(self.getIconFromPath(line))
+                self.ui.appStoreTable.setItem(index, 0, item)
+                # 填充程序路径
+                self.ui.appStoreTable.setItem(index, 1, QTableWidgetItem(line.strip('\n')))
+
+    def getIconFromPath(self, filePath):
+        large, small = win32gui.ExtractIconEx(filePath, 0)
+        win32gui.DestroyIcon(small[0])
+        hdc = win32ui.CreateDCFromHandle(win32gui.GetDC(0))
+        hbmp = win32ui.CreateBitmap()
+        hbmp.CreateCompatibleBitmap(hdc, 32, 32)
+        hdc = hdc.CreateCompatibleDC()
+        hdc.SelectObject(hbmp)
+        hdc.DrawIcon((0, 0), large[0])
+        bmpstr = hbmp.GetBitmapBits(True)
+        img = Image.frombuffer(
+            'RGBA',
+            (32, 32),
+            bmpstr, 'raw', 'BGRA', 0, 1
+        )
+        img.save('../resource/temp/temp.png')
+        icon = QIcon("../resource/temp/temp.png")
+        os.remove("../resource/temp/temp.png")
+        return icon
 
 
 if __name__ == "__main__":
